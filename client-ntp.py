148feab657026ca0bf79d53d7a8eb51769c94176
Awis Qirani <qiraniawis@gmail.com>
2020-12-19 04:14:07 +0800

    Adding localDT and localDTSTr

--- client-ntp.py
+++ client-ntp.py
@@ -3,6 +3,7 @@ import sys
 import json
 import tqdm
 import os
+import datetime
 
 # create client socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
@@ -18,14 +19,25 @@ print(f"[+] Connecting to {host}:{port}")
 s.connect((host, port))
 print("[+] Connected.")
 
-msgFromClient = "Hello, this is client"
-bytesSend = str.encode(msgFromClient)
+# get local sys datetime
+localDT = datetime.datetime.now()
+
+# print local sys datetime
+print("[+] Local System DateTime : ", localDT)
+
+# convert datetime to string
+localDTStr = localDT.strftime("%Y-%m-%d, %H:%M:%S")
+
+#msgFromClient = "Hello, this is client"
+#bytesSend = str.encode(msgFromClient)
+bytesSend = str.encode(localDTStr)
 buffer = 1024
 
 # send msg to server via UDP socket
 s.sendto(bytesSend, (host, port))
 
-print("[+] Sending message to NTP Server")
+
+print("[+] Sending Local DateTime to NTP Server")
 
 # close the socket
 s.close()
