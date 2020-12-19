dfc2e381c9e0c4ccd356a8556e926cec6f6278e5
Awis Qirani <qiraniawis@gmail.com>
2020-12-20 02:32:03 +0800

    Update for calcOffset()

--- client-ntp.py
+++ client-ntp.py
@@ -9,8 +9,39 @@ from os import system
 
 def calcOffset(T1, T2, T3, T4):
 	print("\n[+] Calculating Offset")
-	print(T1, T2, T3, T4)
-	
+	#print(T1, T2, T3, T4) # print string 
+
+	offsetStatusA = 0
+	offsetStatusB = 0
+
+	T1 = datetime.strptime(T1, "%H:%M:%S.%f") # convert string to datetime
+	T2 = datetime.strptime(T2, "%H:%M:%S.%f") # convert string to datetime
+	T3 = datetime.strptime(T3, "%H:%M:%S.%f") # convert string to datetime
+	T4 = datetime.strptime(T4, "%H:%M:%S.%f") # convert string to datetime
+
+	if(T1 >= T2):
+		a = T1 - T2
+		offsetStatusA = 1
+	elif(T2 > T1):
+		a = T2 -T1
+	if(T3 >= T4):
+		b = T3 -T4
+	elif(T4 > T3):
+		b = T4 - T3
+		offsetStatusB = 1
+
+	offset = ((a + b) / 2)
+	offsetSec = offset.total_seconds()
+	if(offsetStatusA == 1 and offsetStatusB == 1):
+		sign = "Haste(+)"
+		print(f"[+] Please adjust the local time by minus {offsetSec} seconds to sync with NTP Server")
+	else:
+		sign = "Delay(-)"
+		print(f"[+] Please adjust the local time by adding {offsetSec} seconds to sync with NTP Server")
+	print("[+] Offset DateTime :", offsetSec, f"seconds [{sign}]") # print offset by seconds
+	#offset = (((T2 - T1) + (T3 - T4)) / 2)
+
+
 
 def calcDelay():
 	print("Calculating Delay")
@@ -80,7 +111,18 @@ def main():
 
 	T4 =  datetime.now() #timestamp reference
 	T4 = T4.strftime("%Y-%m-%d %H:%M:%S.%f")
+	T4 = datetime.strptime(T4, "%Y-%m-%d %H:%M:%S.%f")
 	print("[+] T4 :", T4)
+
+	#print(type(T1))
+	#print(type(T2))
+	#print(type(T3))
+	#print(type(T4))
+
+	T1 = datetime.strftime(T1, "%H:%M:%S.%f") # convert datetime to string
+	T2 = datetime.strftime(T2, "%H:%M:%S.%f") # convert datetime to string
+	T3 = datetime.strftime(T3, "%H:%M:%S.%f") # convert datetime to string
+	T4 = datetime.strftime(T4, "%H:%M:%S.%f") # convert datetime to string
 
 	calcOffset(T1, T2, T3, T4)
 
