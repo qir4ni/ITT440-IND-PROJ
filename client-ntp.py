f130b2e82e203d9e767b6851818d611153f47955
Awis Qirani <qiraniawis@gmail.com>
2020-12-19 16:51:26 +0800

    Minor changes

--- client-ntp.py
+++ client-ntp.py
@@ -69,7 +69,7 @@ def main():
 
 	T2, address = s.recvfrom(buffer) # receive timestamp
 	T2 = datetime.strptime(T2.decode(), "%Y-%m-%d, %H:%M:%S")
-	print(T2)
+	print("T2", T2)
 
 	T3, address = s.recvfrom(buffer)  # transmitted timestamp
 	T3 = datetime.strptime(T3.decode(), "%Y-%m-%d, %H:%M:%S")
